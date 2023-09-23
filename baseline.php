58c6d80cac11f7167c481291dcf7a81c7642da82
tyamahori <tyamahori@gmail.com>
2023-09-23 22:48:42 +0900

    lint

--- baseline.php
+++ baseline.php
@@ -1,30 +1,31 @@
-<?php declare(strict_types = 1);
+<?php
+declare(strict_types = 1);
 
 $ignoreErrors = [];
 $ignoreErrors[] = [
-	'message' => '#^Only booleans are allowed in a ternary operator condition, mixed given\\.$#',
-	'count' => 1,
-	'path' => __DIR__ . '/config/broadcasting.php',
+    'message' => '#^Only booleans are allowed in a ternary operator condition, mixed given\\.$#',
+    'count' => 1,
+    'path' => __DIR__ . '/config/broadcasting.php',
 ];
 $ignoreErrors[] = [
-	'message' => '#^Parameter \\#1 \\$title of static method Illuminate\\\\Support\\\\Str\\:\\:slug\\(\\) expects string, mixed given\\.$#',
-	'count' => 1,
-	'path' => __DIR__ . '/config/cache.php',
+    'message' => '#^Parameter \\#1 \\$title of static method Illuminate\\\\Support\\\\Str\\:\\:slug\\(\\) expects string, mixed given\\.$#',
+    'count' => 1,
+    'path' => __DIR__ . '/config/cache.php',
 ];
 $ignoreErrors[] = [
-	'message' => '#^Parameter \\#1 \\$title of static method Illuminate\\\\Support\\\\Str\\:\\:slug\\(\\) expects string, mixed given\\.$#',
-	'count' => 1,
-	'path' => __DIR__ . '/config/database.php',
+    'message' => '#^Parameter \\#1 \\$title of static method Illuminate\\\\Support\\\\Str\\:\\:slug\\(\\) expects string, mixed given\\.$#',
+    'count' => 1,
+    'path' => __DIR__ . '/config/database.php',
 ];
 $ignoreErrors[] = [
-	'message' => '#^Parameter \\#1 \\$title of static method Illuminate\\\\Support\\\\Str\\:\\:slug\\(\\) expects string, mixed given\\.$#',
-	'count' => 1,
-	'path' => __DIR__ . '/config/session.php',
+    'message' => '#^Parameter \\#1 \\$title of static method Illuminate\\\\Support\\\\Str\\:\\:slug\\(\\) expects string, mixed given\\.$#',
+    'count' => 1,
+    'path' => __DIR__ . '/config/session.php',
 ];
 $ignoreErrors[] = [
-	'message' => '#^Undefined variable\\: \\$this$#',
-	'count' => 1,
-	'path' => __DIR__ . '/routes/console.php',
+    'message' => '#^Undefined variable\\: \\$this$#',
+    'count' => 1,
+    'path' => __DIR__ . '/routes/console.php',
 ];
 
 return ['parameters' => ['ignoreErrors' => $ignoreErrors]];
