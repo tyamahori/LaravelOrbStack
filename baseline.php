6791145b757a8ff0a5409cffa543313662ccda8f
tyamahori <tyamahori@gmail.com>
2023-10-05 23:10:22 +0900

    さらなるPHPStanの高み

--- baseline.php
+++ baseline.php
@@ -1,7 +1,17 @@
 <?php
-declare(strict_types = 1);
+declare(strict_types=1);
 
 $ignoreErrors = [];
+$ignoreErrors[] = [
+    'message' => '#^Throwing checked exception BadMethodCallException in closure\\!$#',
+    'count' => 1,
+    'path' => __DIR__ . '/app/Providers/RouteServiceProvider.php',
+];
+$ignoreErrors[] = [
+    'message' => '#^Throwing checked exception RuntimeException in closure\\!$#',
+    'count' => 2,
+    'path' => __DIR__ . '/app/Providers/RouteServiceProvider.php',
+];
 $ignoreErrors[] = [
     'message' => '#^Only booleans are allowed in a ternary operator condition, mixed given\\.$#',
     'count' => 1,
