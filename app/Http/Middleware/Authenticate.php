b7230197acdde33c612a7bb20e70ede6182a1027
tyamahori <tyamahori@gmail.com>
2023-11-18 14:00:17 +0900

    お試し

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -11,8 +11,12 @@ class Authenticate extends Middleware
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
-    protected function redirectTo(Request $request): ?string
+    protected function redirectTo(Request $request): string|null
     {
-        return $request->expectsJson() ? null : route('login');
+        if ($request->expectsJson()) {
+            return null;
+        }
+
+        return route('login');
     }
 }
