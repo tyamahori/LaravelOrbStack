e46e223c57df67b21c840bf40181e0aca02dc67b
tyamahori <tyamahori@gmail.com>
2023-12-10 18:18:20 +0900

    frankenphp設定対応

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,37 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Config\Repository;
 use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Routing\UrlGenerator;
+use Psr\Log\LoggerInterface;
+use function assert;
+use function is_string;
 
 class HomeController extends Controller
 {
-    public static function home(): Renderable
-    {
+    public function home(
+        Repository $config,
+        UrlGenerator $url,
+        CarbonImmutable $carbonImmutable,
+        LoggerInterface $logger
+    ): Renderable {
+
+        $appUrl = $config->get('app.url');
+        assert(is_string($appUrl), 'appUrl should be string.');
+        $route = $url->route('welcome');
+
+        $logger
+            ->info(
+                'debug',
+                [
+                    $appUrl,
+                    $route,
+                    $carbonImmutable->toString(),
+                ]
+            );
+
         return view('welcome');
     }
 }
