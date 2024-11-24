36a2d01c1f845c5c1e9776289e27ae1d45901564
tyamahori <tyamahori@gmail.com>
2024-11-24 14:23:07 +0900

    wip

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Illuminate\Console\Command;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
@@ -19,6 +20,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command(
     'inspire',
     function (): void {
+        assert($this instanceof Command, '$this should be an instance of Command');
+
         $this->comment(Inspiring::quote());
     }
 )
