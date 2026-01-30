4bed4f31017d0be85510dcd3e6c4c402bc190f32
tyamahori <tyamahori@gmail.com>
2026-01-31 08:06:15 +0900

    update!

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "PHP 8.4 development environment with Composer";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            php84
+            php84Packages.composer
+          ];
+
+          shellHook = ''
+            echo "PHP 8.4 development environment"
+            php --version
+            composer --version
+          '';
+        };
+      }
+    );
+}
